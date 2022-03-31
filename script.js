2b0117d38c38cb088d1ff5b55c5f3a1d7b02b944
Guilherme Comin <182739@upf.br>
2022-03-31 17:27:09 -0300

    Layout segunda tela e início js

--- script.js
+++ script.js
@@ -1,43 +1,58 @@
 // Funções botões trabalho
 function increaseWork() {
     var workValue = document.getElementById('data-work').value
-    document.getElementById('data-work').value = parseInt(workValue)+1
-} 
+    if (workValue < 999) {
+        document.getElementById('data-work').value = parseInt(workValue) + 1
+    }
+}
 function decreaseWork() {
     var workValue = document.getElementById('data-work').value
-    document.getElementById('data-work').value = parseInt(workValue)-1
-} 
+    if (workValue > 1) {
+        document.getElementById('data-work').value = parseInt(workValue) - 1
+    }
+}
 
 // Funções botões pausa
 function increaseRest() {
     var restValue = document.getElementById('data-rest').value
-    document.getElementById('data-rest').value = parseInt(restValue)+1
-} 
+    if (restValue < 999) {
+        document.getElementById('data-rest').value = parseInt(restValue) + 1
+    }
+}
 function decreaseRest() {
     var restValue = document.getElementById('data-rest').value
-    document.getElementById('data-rest').value = parseInt(restValue)-1
-} 
+    if (restValue > 1) {
+        document.getElementById('data-rest').value = parseInt(restValue) - 1
+    }
+}
 
 // Funções botões sessão
 function increaseSession() {
     var sessionValue = document.getElementById('data-session').value
-    document.getElementById('data-session').value = parseInt(sessionValue)+1
-} 
+    if (sessionValue < 999) {
+        document.getElementById('data-session').value = parseInt(sessionValue) + 1
+    }
+}
 function decreaseSession() {
     var sessionValue = document.getElementById('data-session').value
-    document.getElementById('data-session').value = parseInt(sessionValue)-1
+    if (sessionValue > 1) {
+        document.getElementById('data-session').value = parseInt(sessionValue) - 1
+    }
 } 
 
+function startCount() {
+    let minutos = parseInt(document.querySelector('[data-minutes]').innerHTML)
+    let segundos = parseInt(document.querySelector('[data-seconds]').innerHTML)
+    setInterval(() => {
+        while (minutos > 0) {
+            if (segundos == 0) {
 
-// var xhr = new XMLHttpRequest();
-// xhr.open("POST", url, true);
-// xhr.setRequestHeader('Content-Type', 'application/json');
-// xhr.send(JSON.stringify({
-//     name: "Deska",
-//     email: "deska@gmail.com",
-//     phone: "342234553"
-//  }));
-// xhr.onload = function() {
-//     var data = JSON.parse(this.responseText);
-//     console.log(data);
-// };
+            }
+        } 
+    }, 1000);
+}
+
+// setInterval(() => {
+//     i++;
+//     console.log(i)
+// }, 1000);
